e43b4875f50fc5782d5bd67651da1f01bf3ea989
Callum Lovekin <CallumLovekin@callums-mbp-2.client.rave.ac.uk>
2017-10-31 13:05:24 +0000

    New Project

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$('#details').hide();
+
+$('button').click( function() {
+  $('#home').hide();
+  $('#details').show();
+})
+
+
+$('#back').click( function(){
+  $('#home').show();
+  $('#details').hide();
+});
+
+// use jQuery to select the HTML elements we're going to manipulate
+var homeGoButton = $('#home button');
+var homeDropdown = $('#home select');
+var resultsOL = $('#results ol');
+// tell the GO button to do something when we click it
+homeGoButton.click( function()
+{
+  // get user input
+  var selectedOption = homeDropdown.val();
+  // using jQuery val(), see http://api.jquery.com/val
+  console.log('You picked ' + selectedOption);
+  // filter+sort people by user selection
+
+  var filters = [ {key:selectedOption} ]; // array of objects
+  var resultsList = filterAndSortList(peopleList, filters);
+
+  console.log(resultsList);
+
+  // show the results in the #results section
+  	showList(resultsList, resultsOL);
+
+})
